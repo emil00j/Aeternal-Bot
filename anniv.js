6d3e82b63622a17edff8ab203727c37247d41ccc
emil00j <36563971+emil00j@users.noreply.github.com>
2019-05-12 14:55:08 +0200

    Update
    Just for fun lol :)

--- anniv.js
+++ anniv.js
@@ -3,155 +3,125 @@ const pending = require("./pending.js");
 const user = require("./user.js");
 const Discord = require("discord.js");
 const useful = require("./useful.js");
+const anniv = require("./anniv.json");
+const month = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
 
 class AddBirth {
 
     constructor(msg, args) {
         this.msg = msg;
         this.args = args;
+        this.date = ["1", "1", "1900"];
+        this.curStage = 0;
 
-        this.write();
+        this.sendEmbed();
     }
 
-    security(id) {
-        if (this.msg.author.id == id || this.msg.author.id == "251772715256512513") {
-            return false;
+    createEmbed() {
+        let embed = new Discord.RichEmbed()
+            .setFooter(`Demandé par ${this.msg.author.username}`, this.msg.author.avatarURL)
+            .setColor("#12a6c9")
+            .setTitle(`${this.msg.author.username} indiquez votre age.`)
+            .setDescription(`Utilisez les réactions ou indiques les informations dirrectement dans le chat. Puis cliquer sur valider après chaque information correcte indiquée.
+
+day: ${this.date[0]}
+month: ${this.date[1]}
+year: ${this.date[2]}
+
+Tu es né le ${this.date[0]} ${month[Number(this.date[1]) - 1]} ${this.date[2]}.`);
+
+        return embed;
+
+    }
+
+    sendEmbed() {
+        let embed = this.createEmbed();
+        this.msg.channel.send({
+                embed
+            })
+            .then(msg => {
+                this.addReact(msg)
+            })
+            .catch(console.error);
+    }
+
+    action(react, user) {
+        if (user.id == this.msg.author.id) {
+            if (react.emoji.toString() == "▶") {
+                this.date[this.curStage]++;
+            } else if (react.emoji.toString() == "◀") {
+                this.date[this.curStage]--;
+            } else if (react.emoji.toString() == "✅") {
+                this.curStage++;
+            } else if (react.emoji.toString() == "❌") {
+                this.botmessage.clearReactions();
+                this.delete();
+                this.msg.channel.send("Opération en cours annulée.")
+            }
         }
 
-        return true;
+        if (this.curStage == 3) {
+            this.endProcess()
+        }
+
+        react.remove(user);
+        this.editEmbed();
     }
 
-    write() {
-        let id = this.msg.author.id
-        if (this.msg.mentions.members.array().length > 0) {
-          id = this.msg.mentions.members.array()[0].id;
-        } 
-
-        if (this.check(id)) {
-            this.msg.channel.send("Ce membre a déjà renseigné sa date d'anniversaire.");
-        } else if (this.args.length < 1) {
-            this.msg.channel.send("Problème dans la formulation de la date ou dans la mention.");
-        } else if (this.security(id)) {
-            this.msg.channel.send("Tu ne peux pas changer ou intéragir avec l'anniversaire de cette personne.")
-        } else {
-            console.log(`add: ${id}`)
-            let text = this.args[0].split("-")[0];
-            for (let i = 1; i < this.args[0].split("-").length; i++) {
-                let a = this.args[0].split("-")[i];
-
-                if (a.startsWith("0")) {
-                    a = a.slice(1)
-                }
-
-                text += "-" + a
+    endProcess() {
+        if (!useful.checkIn(this.msg.author.id, Object.keys(anniv))) {
+            let obj = {
+                "date": this.date[2] + "-" + this.date[1] + "-" + this.date[0],
+                "last": []
             }
 
-            fs.appendFile("./anniv.txt", `\n${id}:${text}`, (err) => {
-                if (err) throw err;
-            });
-
-            this.msg.channel.send("Ce membre à bien été ajouté à la liste des anniversaires !")
+            anniv[this.msg.author.id] = obj
+        } else {
+            anniv[this.msg.author.id].date = this.date[2] + "-" + this.date[1] + "-" + this.date[0];
         }
 
+        fs.writeFile("./anniv.json", JSON.stringify(anniv), console.error);
+        this.botmessage.clearReactions();
+        this.msg.channel.send("Votre age à été mis à jour.")
         this.delete()
     }
 
-    check(id) {
-        let data = fs.readFileSync("./anniv.txt", "utf-8");
+    editEmbed() {
+        let embed = this.createEmbed();
+        this.botmessage.edit({
+            embed
+        });
+    }
 
-        let s = data.split("\n")
-        for (let i = 0; i < s.length; i++) {
-            let a = s[i].split(":")[0]
-            if (a == id) {
-                return true;
-            }
-        }
+    setInfo(info) {
+        this.date[this.curStage] = info;
+        this.editEmbed();
+    }
 
-        return false;
+    addReact(msg) {
+        this.botmessage = msg
+        msg.react("◀")
+            .then(r1 => {
+                r1.message.react("▶")
+                    .then(r2 => {
+                        r2.message.react("✅")
+                            .then(r3 => {
+                                r3.message.react("❌");
+                            })
+                            .catch(console.error);
+                    })
+                    .catch(console.error);
+            })
+            .catch(console.error);
     }
 
     delete() {
         for (let i = 0; i < pending.pendingAdd.length; i++) {
             if (pending.pendingAdd[i] == this) {
-                pending.pendingAdd.slice(i, 1);
+                pending.pendingAdd.splice(i, 1);
                 break;
             }
         }
-    }
-}
-
-class RemoveBirth {
-
-    constructor(msg, args) {
-        this.msg = msg;
-        this.args = args;
-
-        this.clear();
-    }
-
-    security(id) {
-        if (this.msg.author.id == id || this.msg.author.id == "251772715256512513") {
-            return false;
-        }
-
-        return true;
-    }
-
-    clear() {
-        var id = this.msg.author.id;
-        if (this.msg.mentions.members.array().length > 0) {
-            id = this.msg.mentions.members.array()[0].id
-        }
-
-        if (this.check(id)) {
-            this.msg.channel.send("Ce membre n'a pas indiqué sa date de naissance.")
-        } else if (this.security(id)) {
-            this.msg.channel.send("Tu ne possèdes pas l'autorisation de supprimer les donnés de ce membre.")
-        } else {
-            console.log(`remove: ${id}`)
-            let anniv = fs.readFileSync("./anniv.txt", "utf-8");
-            let s = anniv.split("\n");
-            let stay = "251772715256512513:2000-9-17";
-
-            for (let i = 1; i < s.length; i++) {
-                let a = s[i].split(":")[0]
-                if (a != id) {
-                    stay += "\n" + s[i]
-                }
-            }
-
-            fs.writeFile("./anniv.txt", stay, (err) => {
-                if (err) throw err;
-            });
-
-            this.msg.channel.send("Ce membre à bien été retiré à la liste des anniversaires !");
-
-        }
-
-        this.delete()
-    }
-
-    delete() {
-        for (let i = 0; i < pending.pendingRemove.length; i++) {
-            if (pending.pendingRemove[i] == this) {
-                pending.pendingRemove.slice(i, 1);
-                break;
-            }
-        }
-    }
-
-    check(id) {
-        let data = fs.readFileSync("./anniv.txt", "utf-8");
-
-        let s = data.split("\n")
-        for (let i = 0; i < s.length; i++) {
-            let a = s[i].split(":")[0]
-            if (a == id) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
 
@@ -175,28 +145,26 @@ class SeeBirthdays {
     }
 
     createList() {
-        let data = fs.readFileSync("./anniv.txt", "utf-8")
         this.memberList = new Array;
         for (let i = 0; i < 12; i++) {
             this.memberList.push(new Array);
         }
 
-        let s = data.split("\n")
-        for (let i = 0; i < s.length; i++) {
-            let f = useful.findGuildUser(this.msg.guild.id, s[i].split(":")[0]);
+
+        for (let i = 0; i < Object.keys(anniv).length; i++) {
+            let f = useful.findGuildUser(this.msg.guild.id, Object.keys(anniv)[i]);
 
             if (f !== undefined) {
-                this.memberList[Number(s[i].split(":")[1].split("-")[1]) - 1].push([f.user, s[i].split(":")[1]]);
+                this.memberList[Number(anniv[Object.keys(anniv)[i]].date.split("-")[1]) - 1].push([f.user, anniv[Object.keys(anniv)[i]].date]);
             }
         }
     }
 
     createEmbed() {
-        let mois = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "decembre"];
         let color = ["#088a4b", "#088ab5", "#0174df", "#0040ff", "#2e2efe", "#8258fa", "#be81f7", "#e2a9f3", "#f781f3", "#fa58d0", "#f78181"]
 
         let embed = new Discord.RichEmbed()
-            .setTitle(`Anniversaires du mois de ${mois[this.selected]}`)
+            .setTitle(`Anniversaires du mois de ${month[this.selected]}`)
             .setFooter(`Demandé par ${this.msg.author.username}`, this.msg.author.avatarURL)
             .setColor(color[this.selected])
             .setTimestamp();
@@ -207,7 +175,7 @@ class SeeBirthdays {
 
         for (let i = 0; i < this.memberList[this.selected].length; i++) {
             let m = this.memberList[this.selected][i];
-            embed.addField(m[0].username, `${m[1].split("-")[2]} ${mois[Number(m[1].split("-")[1]) - 1]} ${m[1].split("-")[0]}`);
+            embed.addField(m[0].username, `${m[1].split("-")[2]} ${month[Number(m[1].split("-")[1]) - 1]} ${m[1].split("-")[0]}`);
         }
 
         return embed;
@@ -256,7 +224,7 @@ class SeeBirthdays {
     }
 
     action(react, user) {
-        if (user.id.toString() == this.msg.author.id.toString()) {
+        if (user.id == this.msg.author.id) {
             clearTimeout(this.autodelete);
             this.autodelete = setTimeout(() => {
                 this.delete()
@@ -281,36 +249,24 @@ class SeeBirthdays {
 }
 
 let calcAge = function (id) {
-    let data = fs.readFileSync("./anniv.txt", "utf-8")
-
-    let s = data.split("\n")
+    let t = anniv[id].date;
     let age = 0;
     let d = new Date()
     let date;
 
-    for (let i = 0; i < s.length; i++) {
-        let a = s[i].split(":")[0]
-        if (a == id) {
-            age = d.getFullYear() - Number(s[i].split(":")[1].split("-")[0]) - 1
+    age = d.getFullYear() - Number(t.split("-")[0]) - 1
 
-            if (Number(s[i].split(":")[1].split("-")[1]) < d.getMonth() + 1 || (Number(s[i].split(":")[1].split("-")[1]) == d.getMonth() + 1 && Number(s[i].split(":")[1].split("-")[2]) <= d.getDate())) {
-                age++;
-            }
-
-            date = s[i].split(":")[1];
-            break;
-        }
+    if (Number(t.split("-")[1]) < d.getMonth() + 1 || (Number(t.split("-")[1]) == d.getMonth() + 1 && Number(t.split("-")[2]) <= d.getDate())) {
+        age++;
     }
+
+    date = t;
 
     return [age, date];
 }
 
 exports.addBirth = function (msg, args) {
     pending.pendingAdd.push(new AddBirth(msg, args));
-}
-
-exports.removeBirth = function (msg, args) {
-    pending.pendingRemove.push(new RemoveBirth(msg, args));
 }
 
 exports.seeAge = function (msg, args) {
@@ -345,79 +301,69 @@ exports.seeAge = function (msg, args) {
 }
 
 exports.sayBirth = function (delay) {
-    let alreadySaid = function (id) {
-        let data = fs.readFileSync("./st.txt", "utf-8", err => {
-            if (err) throw err;
-        });
-        console.log(data)
+        let func = () => {
+            let d = new Date()
+            let form = `${d.getMonth() + 1}-${d.getDate()}`
+            let dataChannel = fs.readFileSync("./channel.txt", "utf-8")
 
-        console.log(data.split(":"))
-        console.log(id)
-        if (data.split(":").indexOf(id) >= 0) {
-            return true;
-        }
+            for (let i = 0; i < user.u.guilds.array().length; i++) {    
+                for (k = 0; k < dataChannel.split("\n").length; k++) {
+                    if (dataChannel.split("\n")[k].split(":")[0] == user.u.guilds.array()[i].id) {
+                        var chan = useful.findChannel(user.u.guilds.array()[i].id, dataChannel.split("\n")[k].split(":")[1]);
+                    }
+                }
+                
+                let arr = user.u.guilds.array()[i].members.array();
+                for (j = 0; j < arr.length; j++) {
+                    let u = arr[j]
+                        if (useful.checkIn(u.id, Object.keys(anniv))) {
+                            let obj = anniv[u.id];
 
-        return false;
-    }
-
-    let func = () => {
-        let d = new Date()
-        let form = `${d.getMonth() + 1}-${d.getDate()}`
-        let dataChannel = fs.readFileSync("./channel.txt", "utf-8")
-
-        for (i = 0; i < user.u.guilds.array().length; i++) {
-            for (j = 0; j < dataChannel.split("\n").length; j++) {
-                if (user.u.guilds.array()[i].id == dataChannel.split("\n")[j].split(":")[0]) {
-                    let guild = user.u.guilds.array()[i]
-
-                    for (k = 0; k < guild.channels.array().length; k++) {
-                        if (guild.channels.array()[k].id == dataChannel.split("\n")[j].split(":")[1]) {
-                            let chan = guild.channels.array()[k]
-
-                            fs.readFile("./anniv.txt", "utf-8", (err, data) => {
-                                if (err) throw err;
-
-                                let s = data.split("\n");
-                                for (l = 0; l < s.length; l++) {
-                                    let todayDate = s[l].split(":")[1].split("-")[1] + "-" + s[l].split(":")[1].split("-")[2]
-
-                                    if (todayDate == form && useful.findGuildUser(guild.id, s[l].split(":")[0]) !== undefined && !(alreadySaid(s[l].split(":")[0]))) {
-                                        chan.send(`:gift: Joyeux anniversaire à <@${s[l].split(":")[0]}> qui fête aujourd'hui ses ${calcAge(s[l].split(":")[0])[0]} ans :tada:`);
-                                        fs.appendFile("./st.txt", ":" + s[l].split(":")[0], err => {
-                                            if (err) throw err;
-                                        })
-                                    }
+                            if (!useful.checkIn(d.getFullYear(), obj.last)) {
+                                if (form == `${obj.date.split("-")[1]}-${obj.date.split("-")[2]}`) {
+                                    anniv[u.id].last.push(d.getFullYear());
+                                    chan.send(`Joyeux anniversaire à <@${u.id}> qui fête ses ${calcAge(u.id)[0]} ans ! :tada:`);
+                                    fs.writeFile("./anniv.json", JSON.stringify(anniv), console.error)
                                 }
-                            });
+                            }
                         }
                     }
                 }
             }
+
+            func()
+            setInterval(() => func, delay)
         }
-    }
 
-    func()
-    setInterval(() => func, delay)
-}
+        exports.checkMsg = function (msg) {
+            for (i = 0; i < pending.pendingAdd.length; i++) {
+                let obj = pending.pendingAdd[i];
 
-exports.SeeBirthdays = function (msg, args) {
-    pending.pendingSee.push(new SeeBirthdays(msg, args));
-}
+                if (obj.msg.author.id == msg.author.id && obj.msg.channel.id == msg.channel.id && Number(msg.content) != NaN) {
+                    obj.setInfo(msg.content);
+                    msg.delete();
+                }
+            }
+        }
 
-exports.setChannel = function (msg, args) {
-    if (msg.author.id == "251772715256512513") {
-        fs.appendFile("./channel.txt", `\n${msg.guild.id}:${msg.channel.id}`, (err) => {
-            console.error(err)
-        })
-        msg.channel.send("Channel définie comme channel d'annonce d'anniversaire.")
-            .then((m) => {
-                setTimeout(() => {
-                    m.delete();
-                }, 5000);
-                msg.delete()
-            })
+        exports.seeBirthdays = function (msg, args) {
+            pending.pendingSee.push(new SeeBirthdays(msg, args));
+        }
 
-    } else {
-        msg.channel.send("Vous n'avez pas les droits.")
-    }
-}
+        exports.setChannel = function (msg, args) {
+            if (msg.author.id == "251772715256512513") {
+                fs.appendFile("./channel.txt", `\n${msg.guild.id}:${msg.channel.id}`, (err) => {
+                    console.error(err)
+                })
+                msg.channel.send("Channel définie comme channel d'annonce d'anniversaire.")
+                    .then((m) => {
+                        setTimeout(() => {
+                            m.delete();
+                        }, 5000);
+                        msg.delete()
+                    })
+
+            } else {
+                msg.channel.send("Vous n'avez pas les droits.")
+            }
+        }
